316c5672cf976b019d53dd132630147f350ba437
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-30 19:53:47 -0300

    style: :art: Adicionando terceiro = na comparação

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -118,7 +118,7 @@ class StrangerThings extends React.Component {
       >
         <div className="content strangerfy">
           {
-            process.env.TYPE_USER == 'development' && <h1>Em desenvolvimento</h1>
+            process.env.TYPE_USER === 'development' && <h1>Em desenvolvimento</h1>
           }
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
